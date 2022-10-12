8e39cbdcf6812585812ca5ad2290f7bcf2a5779e
Bryan Hach <84881852+bphach@users.noreply.github.com>
2022-10-12 12:07:25 -0400

    commit

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Inventory {
-    String[] Product = {"Books","CD","MP4"};
-
-        
-
+    String[] Product = {"Books","CD","DVD"};
+    Product[0] = "Books";
+    Product[1] = "CD";
+    Product[2] = "DVD";
 }
